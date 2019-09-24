acf948549b2440397898386fa2d1719ad627ce1e
Viacheslav Kozlov <slavikkozlov3@gmail.com>
2019-09-24 12:09:46 +0300

    remove unused code

--- src/main/java/com/edvantis/rssreader/annotation/LogExecutionTime.java
+++ src/main/java/com/edvantis/rssreader/annotation/LogExecutionTime.java
@@ -2,11 +2,9 @@ package com.edvantis.rssreader.annotation;
 
 import java.lang.annotation.*;
 
-import org.springframework.stereotype.Component;
 
-@Component
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface LogExecutionTime {
-	
+	 String executionTime() default "";
 }
